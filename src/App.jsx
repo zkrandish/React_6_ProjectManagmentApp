7ab89461781c82e73b3381f44832bf0bef1bf1d8
Zahra Karandish <zkarandish@gmail.com>
2025-03-10 12:48:10 -0400

    addProject

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,22 @@ function App() {
     });
   }
 
+  function handleAddProject(projectData){
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject ]
+      }
+    })
+  }
+
   let content;
   if(projectsState.selectedProjectId === null){
-    content = <NewProject/>
+    content = <NewProject onAdd={handleAddProject}/>
   } else if( projectsState.selectedProjectId === undefined){
     content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
   }
