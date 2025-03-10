3f9f3a8f06130d4f7c5bada28bc9fb248de83198
Zahra Karandish <zkarandish@gmail.com>
2025-03-10 11:10:21 -0400

    addProject button conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSideBar from "./components/ProjectsSideBar.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject(){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+
+      }
+    });
+  }
+
+  let content;
+  if(projectsState.selectedProjectId === null){
+    content = <NewProject/>
+  } else if( projectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar/>
-      <NoProjectSelected/>
+      <ProjectsSideBar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
