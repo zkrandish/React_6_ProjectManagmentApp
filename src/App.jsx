eda1933d84d933cb0340033fcd44bd528943ef5a
Zahra Karandish <zkarandish@gmail.com>
2025-03-10 10:21:57 -0400

    NewProject and Input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from "./components/NewProject.jsx";
 import ProjectsSideBar from "./components/ProjectsSideBar.jsx";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSideBar/>
+      <NewProject/>
     </main>
   );
 }
