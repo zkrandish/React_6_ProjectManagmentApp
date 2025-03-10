df0814bae95c08a083bfb8fb041202c9d7b3d5c1
Zahra Karandish <zkarandish@gmail.com>
2025-03-10 10:55:43 -0400

    noProject component fallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSideBar from "./components/ProjectsSideBar.jsx";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSideBar/>
-      <NewProject/>
+      <NoProjectSelected/>
     </main>
   );
 }
