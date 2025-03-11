934e3d99122555eb9fcd6c16d477666e268fbe45
Zahra Karandish <zkarandish@gmail.com>
2025-03-11 09:54:24 -0400

    delete a project

--- src/App.jsx
+++ src/App.jsx
@@ -49,11 +49,23 @@ function App() {
       };
     });
   }
+  function handleDelete() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+
   const selectedProject = projectsState.projects.find(
     (project) => project.id === projectsState.selectedProjectId
   );
 
-  let content = <SelectedProject project={selectedProject} />;
+  let content = <SelectedProject project={selectedProject} onDelete={handleDelete}/>;
 
   if (projectsState.selectedProjectId === null) {
     content = (
